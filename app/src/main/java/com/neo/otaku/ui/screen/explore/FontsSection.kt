f11dfac384f65c933dd78fbe9e85e55ae9f3ea72
Irineu333 <mmmirineusilva@gmail.com>
2022-10-24 03:06:55 -0300

    feat(explore): improve card ripple effect

--- app/src/main/java/com/neo/otaku/ui/screen/explore/FontsSection.kt
+++ app/src/main/java/com/neo/otaku/ui/screen/explore/FontsSection.kt
@@ -1,21 +1,24 @@
 package com.neo.otaku.ui.screen.explore
 
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.BrokenImage
 import androidx.compose.material.icons.twotone.Download
 import androidx.compose.material.icons.twotone.Image
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
@@ -43,13 +46,13 @@ fun FontsSection(
             Icon(
                 imageVector = Icons.TwoTone.Download,
                 contentDescription = null,
-                tint = MaterialTheme.colorScheme.primary,
+                tint = colorScheme.primary,
                 modifier = Modifier
                     .padding(4.dp)
                     .size(18.dp)
             )
 
-            Text(text = "Fontes")
+            Text(text = "Fontes", color = contentColorFor(colorScheme.background))
         }
 
         Spacer(modifier = Modifier.weight(1f))
@@ -59,46 +62,54 @@ fun FontsSection(
         }
     }
 
-    LazyColumn {
-        itemPadding(
-            items = fonts,
-            paddingBottom = 4.dp
-        ) { font ->
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .roundedShape(4.dp)
-                    .clickable {}
-                    .padding(2.dp)
-            ) {
-                SubcomposeAsyncImage(
-                    model = font.iconUrl,
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.circleShape(),
-                    loading = {
-                        Icon(
-                            imageVector = Icons.TwoTone.Image,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.primary
-                        )
-                    },
-                    error = {
-                        Icon(
-                            imageVector = Icons.TwoTone.BrokenImage,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.primary
-                        )
-                    },
-                )
+    CompositionLocalProvider(
+        LocalIndication provides rememberRipple(
+            color = colorScheme.primary
+        )
+    ) {
+        LazyColumn {
+            itemPadding(
+                items = fonts,
+                paddingBottom = 4.dp
+            ) { font ->
 
-                Spacer(modifier = Modifier.width(8.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .roundedShape(4.dp)
+                        .clickable {}
+                        .padding(2.dp)
+                ) {
+                    SubcomposeAsyncImage(
+                        model = font.iconUrl,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.circleShape(),
+                        loading = {
+                            Icon(
+                                imageVector = Icons.TwoTone.Image,
+                                contentDescription = null,
+                                tint = colorScheme.primary
+                            )
+                        },
+                        error = {
+                            Icon(
+                                imageVector = Icons.TwoTone.BrokenImage,
+                                contentDescription = null,
+                                tint = colorScheme.primary
+                            )
+                        },
+                    )
 
-                Text(
-                    text = font.name,
-                    fontSize = 18.sp,
-                    modifier = Modifier.padding(end = 4.dp)
-                )
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Text(
+                        text = font.name,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(end = 4.dp),
+                        color = contentColorFor(colorScheme.background)
+                    )
+                }
             }
         }
     }
